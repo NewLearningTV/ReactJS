b689aebe249125d06e5c974d248213cff533745b
NewLearningTV 뉴러닝 <129813107+NewLearningTV@users.noreply.github.com>
2023-06-27 19:45:53 -0400

    App.jsx 업데이트

--- App.jsx
+++ App.jsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+const title = 'React';
+
 function App() {
     return (
         <div>
-            <h1>Hello React - 리액트 테스트입니다!</h1>
+            <h1>Hello {title} - 리액트 테스트입니다!</h1>
+            
+            {/* JSX의 htmlFor는 자바스크립트에 더 가깝습니다 */}
+            <label htmlFor="search">검색창: </label>
+            <input id="search" type="text" />
         </div>
     );
 }
