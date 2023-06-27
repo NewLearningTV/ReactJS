ee12c7d5bd92dee12f18253553c21e8ca32a8f14
NewLearningTV 뉴러닝 <129813107+NewLearningTV@users.noreply.github.com>
2023-06-27 19:49:41 -0400

    App.jsx에 function 추가

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 
-const title = 'React';
+const welcome = {
+    intro: '안녕하세요',
+    title: '리액트',
+};
+
+function getTest(text) {
+    return text;
+}
 
 function App() {
     return (
         <div>
-            <h1>Hello {title} - 리액트 테스트입니다!</h1>
+            <h1>{welcome.intro} - {welcome.title} {getTest('테스트입니다!')}</h1>
             
             {/* JSX의 htmlFor는 자바스크립트에 더 가깝습니다 */}
             <label htmlFor="search">검색창: </label>
