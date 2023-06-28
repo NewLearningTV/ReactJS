7e0581e9a95e7a83d75aa2d75b32101c1baeb689
NewLearningTV 뉴러닝 <129813107+NewLearningTV@users.noreply.github.com>
2023-06-27 20:09:54 -0400

    listToDo map도 추가

--- App.jsx
+++ App.jsx
@@ -9,6 +9,21 @@ function getTest(text) {
     return text;
 }
 
+const listToDo = [
+    {
+        taskName: '청소하기',
+        frequency: '1주일에 한번',
+    },
+    {
+        taskName: '개발공부하기',
+        frequency: '1주일에 5번',
+    },
+    {
+        taskName: '요리하기',
+        frequency: '1주일에 3번',
+    }
+]
+
 function App() {
     return (
         <div>
@@ -17,6 +32,13 @@ function App() {
             {/* JSX의 htmlFor는 자바스크립트에 더 가깝습니다 */}
             <label htmlFor="search">검색창: </label>
             <input id="search" type="text" />
+
+            <ul>
+                {listToDo.map(function (item) {
+                    return <li key={item}>{item.taskName}, {item.frequency}</li>;
+                })}
+            </ul>
+
         </div>
     );
 }
